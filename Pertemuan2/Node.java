55479a84971f6874cfd05bcd6295948e14d2e0b3
Rama Rizki <rama23rizki04@gmail.com>
2025-02-18 12:10:14 +0700

    commit

---
+++ Pertemuan2/Node.java
@@ -0,0 +1,29 @@
+package pertemuan2;
+
+public class Node {
+	private int nilai;
+	private Node next;
+	
+	public Node(int nilai) {
+		this.nilai = nilai;
+	}
+	
+	public void setNilai (int nilai) {
+		this.nilai=nilai;
+		
+	}
+	
+	public int getnilai() {
+		return nilai;
+	}
+	
+	public void setnext (Node next) {
+		this.next = next;
+		
+	}
+	
+	public Node getnext() {
+		return next;
+	}
+	
+}
---
+++ Pertemuan2/NodeMain.java
@@ -0,0 +1,28 @@
+package pertemuan2;
+
+public class NodeMain {
+	public static void main (String[] args) {
+		
+		// Membuat 5 buah node
+		Node n1 = new Node (5);
+		Node n2 = new Node (7);
+		Node n3 = new Node (9);
+		Node n4 = new Node (2);
+		Node n5 = new Node (3);
+
+		
+		// Membuat relasi antar Node 
+		n4.setnext(n5);
+		n5.setnext(n1);
+		n1.setnext(n2);
+		n2.setnext(n3);
+		
+		// Menampilkan Node dengan Pointer p
+		Node p = n4;
+		while(p != null)
+		{
+			System.out.printf("%d ", p.getnilai());
+			p = p.getnext();
+		}
+	}
+}
